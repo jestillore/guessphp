7f5ca3c71fbca002778db5dc86e44e23d0e7e97a
Jillberth Estillore <ejillberth@gmail.com>
2021-08-16 18:21:32 +0800

    First commit

---
+++ guessphp.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+PHP80="/usr/local/Cellar/php/8.0.9/bin/php"
+PHP74="/usr/local/Cellar/php@7.4/7.4.22/bin/php"
+PHP72="/usr/local/Cellar/php@7.2/7.2.34_4/bin/php"
+
+CWD=$('pwd')
+FILE="${CWD}/composer.json"
+PHP="/usr/local/bin/php"
+
+if [ -f "$FILE" ]; then
+    VERSION=$(jq '.require.php' composer.json)
+    if [ -n "$VERSION" ]; then
+        case "$VERSION" in
+            *8.0*) PHP=$PHP80 ;;
+            *7.4*) PHP=$PHP74 ;;
+            *7.2*) PHP=$PHP72 ;;
+        esac
+    fi
+fi
+
+$PHP "$@"
