0c8bd5a7c8c2d8359f1e9130c52e4f12f2050c36
Jillberth Estillore <ejillberth@gmail.com>
2022-10-20 17:12:40 +0800

    Support .guessphp file

--- guessphp.sh
+++ guessphp.sh
@@ -6,17 +6,21 @@ PHP72="/usr/local/Cellar/php@7.2/7.2.34_4/bin/php"
 PHP="/usr/local/bin/php"
 
 CWD=$('pwd')
-FILE="${CWD}/composer.json"
-
+FILE="${CWD}/.guessphp"
 if [ -f "$FILE" ]; then
+    VERSION=$(cat "$FILE")
+fi
+
+if [ -z "$VERSION" ]; then
     VERSION=$(jq '.require.php' composer.json)
-    if [ -n "$VERSION" ]; then
-        case "$VERSION" in
-            *8.0*) PHP=$PHP80 ;;
-            *7.4*) PHP=$PHP74 ;;
-            *7.2*) PHP=$PHP72 ;;
-        esac
-    fi
+fi
+
+if [ -n "$VERSION" ]; then
+    case "$VERSION" in
+        *8.0*) PHP=$PHP80 ;;
+        *7.4*) PHP=$PHP74 ;;
+        *7.2*) PHP=$PHP72 ;;
+    esac
 fi
 
 $PHP "$@"
